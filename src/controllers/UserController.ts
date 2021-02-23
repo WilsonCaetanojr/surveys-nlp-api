c0bd4c182382940f933923118fb9c3701c1745ac
WilsonCaetanojr <wilsonjr_caetano@hotmail.com>
2021-02-23 13:03:36 -0300

    Configura controllers e rotas

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,9 @@
+import {Request, Response} from "express"
+
+class UserController {
+    async create(req:Request , res: Response){
+        const body = req.body
+    }
+}
+
+export {UserController}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import "reflect-metadata"
 import express from "express";
 import "./database"
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  return res.json({});
-});
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => console.log("Running"));
