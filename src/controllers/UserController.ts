79b7fee2dae754beaf1f6985bbddb48cec52a490
WilsonCaetanojr <wilsonjr_caetano@hotmail.com>
2021-02-23 13:37:38 -0300

    Adiciona regra de negócio no UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -8,13 +8,19 @@ class UserController {
 
         const usersRepository = getRepository(User)
 
+        const userAlreadyExists = await usersRepository.findOne({email})
+
+        if(userAlreadyExists) {
+            return res.status(400).json({error: "User already exists!"})
+        }
+
        const user =  usersRepository.create({
            name, email
        })
 
        await usersRepository.save(user)
 
-        return res.send("")
+        return res.send(user)
     }
 }
 
