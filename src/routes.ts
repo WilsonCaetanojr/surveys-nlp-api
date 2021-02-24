5e530ed91cd020008bf359d38fa64a8cd96cb8b3
WilsonCaetanojr <wilsonjr_caetano@hotmail.com>
2021-02-24 08:46:14 -0300

    Configura nova rota

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import {Router} from "express"
 import { UserController } from "./controllers/UserController"
+import { ServeyController } from "./controllers/SurveyController"
 
 const router = Router();
 
 const userController = new UserController();
+const serveyController = new ServeyController();
 
 router.post("/users", userController.create);
+router.post("/serveys", serveyController.create);
 
 
 export {router}
