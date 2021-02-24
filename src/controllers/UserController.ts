7c505c748503ea6440470174f832421ffb615d15
WilsonCaetanojr <wilsonjr_caetano@hotmail.com>
2021-02-24 08:43:57 -0300

    Abstrai repositórios do controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
-import {Request, Response} from "express"
-import { getRepository } from "typeorm"
-import { User } from "../models/User"
+import { Request, Response } from "express"
+import { getCustomRepository } from "typeorm"
+import { UsersRepository } from "../repositories/UsersRepository"
 
 class UserController {
     async create(req:Request , res: Response){
         const {name, email} = req.body
 
-        const usersRepository = getRepository(User)
+        const usersRepository = getCustomRepository(UsersRepository)
 
         const userAlreadyExists = await usersRepository.findOne({email})
 
@@ -24,4 +24,4 @@ class UserController {
     }
 }
 
-export {UserController}
+export { UserController }
