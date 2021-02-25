1c17f6a8076d2650d44db17c69f3968995b295f1
WilsonCaetanojr <wilsonjr_caetano@hotmail.com>
2021-02-25 14:29:59 -0300

    Cria rota e controller para envio de email

---
+++ src/controllers/SendMailController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express"
+import { getCustomRepository } from "typeorm"
+import { SurveysRepository } from "../repositories/SurveysRepository"
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository"
+import { UsersRepository } from "../repositories/UsersRepository"
+
+class SendMailController {
+    async execute(req:Request , res: Response){
+        const {email, survey_id} = req.body
+
+        const usersRepository = getCustomRepository(UsersRepository);
+        const surveysRepository = getCustomRepository(SurveysRepository)
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
+
+        const userExists = await usersRepository.findOne({email})
+
+        if(!userExists){
+            return res.status(400).json({error:"User does not exists."})
+        }
+
+        const surveysExists = await surveysRepository.findOne({id: survey_id})
+
+        if(!surveysExists){
+            return res.status(400).json({error: "Survey does not exists."})
+        }
+
+        const surveyUser = surveysUsersRepository.create({
+            user_id: userExists.id,
+            survey_id
+        })
+
+        await surveysUsersRepository.save(surveyUser)
+
+        return res.json(surveyUser)
+    }
+}
+
+export { SendMailController }
--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,19 @@
 import {Router} from "express"
 import { UserController } from "./controllers/UserController"
 import { ServeyController } from "./controllers/SurveyController"
+import { SendMailController } from "./controllers/SendMailController"
 
 const router = Router();
 
 const userController = new UserController();
 const serveyController = new ServeyController();
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 
 router.post("/serveys", serveyController.create);
 router.get("/serveys", serveyController.show);
 
+router.post("/sendMail", sendMailController.execute);
 
 export {router}
